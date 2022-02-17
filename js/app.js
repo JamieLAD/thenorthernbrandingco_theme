157331ab43a3d8341d05e01c38dc431577c2c3df
JamieApp4 <jamie@app4.co.uk>
2022-02-17 13:15:59 +0000

    Started Adding Animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-feather.replace()
+feather.replace();
+
+butter.init({
+   wrapperId: 'butter',
+   cancelOnTouch: true,
+   wrapperDamper: 0.06
+});
+
+gsap.registerPlugin();
+
+gsap.timeline({
+   scrollTrigger: {
+     trigger: ".about",
+     start: "top bottom",
+     end: "top top",
+     scrub: true,
+     duration: 1
+   },
+   defaults: { ease: Power3.easeInOut }
+ })
+ .from(".about .tag", {y: -100})
+ .from(".about .tag__underline", {scaleX: 0})
+// .fromTo(".tag", { y: -100 }, { y: 0 });
